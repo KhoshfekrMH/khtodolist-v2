6a8219ec6c5030eba23ecf3a7aa2b3d5d6e725e5
Khoshfekr MH <97754590+KhoshfekrMH@users.noreply.github.com>
2022-09-21 23:51:59 +0430

    mongoDB atlas

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://admin-Khoshfekr:64gsF2k4G9v0AN58@cluster0.enpd2tj.mongodb.net/todolistDB");
 
 const itemsSchema = {
   name: String
@@ -137,4 +137,5 @@ app.get("/about", function(req, res){
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
+  console.log()
 });
